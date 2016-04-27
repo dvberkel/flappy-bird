61d358f2458e7a21d5a590f6826d8198d832a6d4
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-04-27 14:22:50 +0200

    Draw a bird

--- js/application.js
+++ js/application.js
@@ -7,8 +7,58 @@
     var context = canvas.getContext('2d');
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    var bird = new codefest.Asset('assets/bird.png');
-    bird.on('ready', function(){
-        context.drawImage(this.image, 0, 0);
-    });
+    var frame = window.frame = {
+        "bird": {
+            "x": canvas.width/2,
+            "y": canvas.height/2,
+            "vx": 1,
+            "vy": 0
+        }
+    };
+    var assets = [
+        new codefest.Asset('assets/bird.png'),
+        new codefest.Asset('assets/ceiling.png'),
+        new codefest.Asset('assets/font_big_0.png'),
+        new codefest.Asset('assets/font_big_1.png'),
+        new codefest.Asset('assets/font_big_2.png'),
+        new codefest.Asset('assets/font_big_3.png'),
+        new codefest.Asset('assets/font_big_4.png'),
+        new codefest.Asset('assets/font_big_5.png'),
+        new codefest.Asset('assets/font_big_6.png'),
+        new codefest.Asset('assets/font_big_7.png'),
+        new codefest.Asset('assets/font_big_8.png'),
+        new codefest.Asset('assets/font_big_9.png'),
+        new codefest.Asset('assets/font_small_0.png'),
+        new codefest.Asset('assets/font_small_1.png'),
+        new codefest.Asset('assets/font_small_2.png'),
+        new codefest.Asset('assets/font_small_3.png'),
+        new codefest.Asset('assets/font_small_4.png'),
+        new codefest.Asset('assets/font_small_5.png'),
+        new codefest.Asset('assets/font_small_6.png'),
+        new codefest.Asset('assets/font_small_7.png'),
+        new codefest.Asset('assets/font_small_8.png'),
+        new codefest.Asset('assets/font_small_9.png'),
+        new codefest.Asset('assets/land.png'),
+        new codefest.Asset('assets/medal_bronze.png'),
+        new codefest.Asset('assets/medal_gold.png'),
+        new codefest.Asset('assets/medal_platinum.png'),
+        new codefest.Asset('assets/medal_silver.png'),
+        new codefest.Asset('assets/pipe-down.png'),
+        new codefest.Asset('assets/pipe.png'),
+        new codefest.Asset('assets/pipe-up.png'),
+        new codefest.Asset('assets/replay.png'),
+        new codefest.Asset('assets/scoreboard.png'),
+        new codefest.Asset('assets/sky.png'),
+        new codefest.Asset('assets/splash.png'),
+        new codefest.Asset('assets/thumb.png'),
+    ];
+
+    function draw(frame){
+        var bird = assets[0].image;
+        context.fillRect(0, 0, canvas.width, canvas.height);
+        var w = bird.width;
+        var h = bird.height/4;
+        context.drawImage(bird, 0, h * 0, w, h, frame.bird.x, frame.bird.y, w, h);
+    };
+    draw(frame);
 })(codefest = codefest || {});
