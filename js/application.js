18ec3f7702482c1316252416fc375954e0a4097d
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-04-27 08:16:48 +0200

    Create a black canvas

--- js/application.js
+++ js/application.js
@@ -1,3 +1,9 @@
 (function(){
     console.log('Get Ready!');
+
+    var canvas = document.getElementById('viewport');
+    canvas.width = 640;
+    canvas.height = 480;
+    var context = canvas.getContext('2d');
+    context.fillRect(0, 0, 640, 480);
 })();
