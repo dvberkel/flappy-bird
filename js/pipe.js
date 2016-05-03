d4d57121d6d451b3192a7244f24521c9c10d5110
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-03 13:49:59 +0200

    Pass options into pipe

--- js/pipe.js
+++ js/pipe.js
@@ -3,20 +3,22 @@
     var pipe_image_asset = new $.Asset('assets/pipe.png');
     var pipe_down_asset = new $.Asset('assets/pipe-down.png');
 
-    var Pipe = $.Pipe = function(x, y){
+    var Pipe = $.Pipe = function(x, y, options){
+        this.options = $.extend(options || {}, {
+            'pipe_top_height': 25,
+            'pipe_gap': 40
+        });
         this.x = x;
         this.y = y;
     };
     Pipe.prototype.draw = function(canvas, context, frame, frame_index, timeline){
-        var pipe_top_height = 25;
-        var pipe_gap = 40;
         var pipe_up = pipe_up_asset.image;
         var pipe_image = pipe_image_asset.image;
         var pipe_down = pipe_down_asset.image;
         var delta = frame.bird.vx * frame_index;
-        context.drawImage(pipe_image, 0, 0, pipe_image.width, pipe_image.height, this.x - delta, 0, pipe_image.width, this.y - pipe_gap);
-        context.drawImage(pipe_image, 0, 0, pipe_image.width, pipe_image.height, this.x - delta, this.y + pipe_gap, pipe_image.width, canvas.height - this.y + pipe_gap);
-        context.drawImage(pipe_down, 0, 0, pipe_down.width, pipe_down.height, this.x - delta, this.y - pipe_gap - pipe_top_height, pipe_down.width, pipe_top_height);
-        context.drawImage(pipe_up, 0, 0, pipe_up.width, pipe_up.height, this.x - delta, this.y + pipe_gap, pipe_up.width, pipe_top_height);
+        context.drawImage(pipe_image, 0, 0, pipe_image.width, pipe_image.height, this.x - delta, 0, pipe_image.width, this.y - this.options.pipe_gap);
+        context.drawImage(pipe_image, 0, 0, pipe_image.width, pipe_image.height, this.x - delta, this.y + this.options.pipe_gap, pipe_image.width, canvas.height - this.y + this.options.pipe_gap);
+        context.drawImage(pipe_down, 0, 0, pipe_down.width, pipe_down.height, this.x - delta, this.y - this.options.pipe_gap - this.options.pipe_top_height, pipe_down.width, this.options.pipe_top_height);
+        context.drawImage(pipe_up, 0, 0, pipe_up.width, pipe_up.height, this.x - delta, this.y + this.options.pipe_gap, pipe_up.width, this.options.pipe_top_height);
     };
 })(codefest = codefest || {});
