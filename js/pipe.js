74604c62c900865c9f1204cbfd003c117cc2475b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-03 15:00:11 +0200

    Create a bounding box method

--- js/pipe.js
+++ js/pipe.js
@@ -12,26 +12,36 @@
         this.y = y;
     };
     Pipe.prototype.draw = function(canvas, context, frame, frame_index, timeline){
-        this.draw_pipe_down(canvas, context, frame, frame_index, timeline);
-        this.draw_pipe_up(canvas, context, frame, frame_index, timeline);
+        var bb = this.bounding_box(canvas, context, frame, frame_index, timeline);
+        this.draw_pipe_down(context, bb.down);
+        this.draw_pipe_up(context, bb.up);
     };
-    Pipe.prototype.draw_pipe_down = function(canvas, context, frame, frame_index, timeline){
+    Pipe.prototype.bounding_box = function(canvas, context, frame, frame_index, timeline){
+        var delta = frame.bird.vx * frame_index;
+       return {
+           'down': {
+               x: this.x - delta,
+               y: 0,
+               w: pipe_image_asset.image.width,
+               h: this.y - this.options.pipe_gap
+           },
+           'up': {
+               x: this.x - delta,
+               y: this.y + this.options.pipe_gap,
+               w: pipe_image_asset.image.width,
+               h: canvas.height - (this.y + this.options.pipe_gap)
+           }
+        };
+    };
+    Pipe.prototype.draw_pipe_down = function(context, bb){
         var pipe_image = pipe_image_asset.image;
         var pipe_down = pipe_down_asset.image;
-        var delta = frame.bird.vx * frame_index;
-        var x = this.x - delta;
-        var y = this.y - this.options.pipe_gap;
-        var bb = { x: x, y: 0, w: pipe_image.width, h: y };
         context.drawImage(pipe_image, bb.x, bb.y, bb.w, bb.h);
         context.drawImage(pipe_down, bb.x, bb.h - this.options.pipe_top_height, bb.w, this.options.pipe_top_height);
     };
-    Pipe.prototype.draw_pipe_up = function(canvas, context, frame, frame_index, timeline){
+    Pipe.prototype.draw_pipe_up = function(context, bb){
         var pipe_up = pipe_up_asset.image;
         var pipe_image = pipe_image_asset.image;
-        var delta = frame.bird.vx * frame_index;
-        var x = this.x - delta;
-        var y = this.y + this.options.pipe_gap;
-        var bb = { x: x, y: y, w: pipe_image.width, h: canvas.height - y };
         context.drawImage(pipe_image, bb.x, bb.y, bb.w, bb.h);
         context.drawImage(pipe_up, bb.x, bb.y, bb.w, this.options.pipe_top_height);
     };
