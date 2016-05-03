ea85c6b0ede44d54d0abc0e3cd2f1328c6e2a509
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-03 14:15:56 +0200

    Clean up pipe drawing

--- js/pipe.js
+++ js/pipe.js
@@ -19,14 +19,18 @@
         var pipe_image = pipe_image_asset.image;
         var pipe_down = pipe_down_asset.image;
         var delta = frame.bird.vx * frame_index;
-        context.drawImage(pipe_image, 0, 0, pipe_image.width, pipe_image.height, this.x - delta, 0, pipe_image.width, this.y - this.options.pipe_gap);
-        context.drawImage(pipe_down, 0, 0, pipe_down.width, pipe_down.height, this.x - delta, this.y - this.options.pipe_gap - this.options.pipe_top_height, pipe_down.width, this.options.pipe_top_height);
+        var x = this.x - delta;
+        var y = this.y - this.options.pipe_gap - this.options.pipe_top_height;
+        context.drawImage(pipe_image, x, 0, pipe_image.width, y);
+        context.drawImage(pipe_down, x, y, pipe_down.width, this.options.pipe_top_height);
     };
     Pipe.prototype.draw_pipe_up = function(canvas, context, frame, frame_index, timeline){
         var pipe_up = pipe_up_asset.image;
         var pipe_image = pipe_image_asset.image;
         var delta = frame.bird.vx * frame_index;
-        context.drawImage(pipe_image, 0, 0, pipe_image.width, pipe_image.height, this.x - delta, this.y + this.options.pipe_gap, pipe_image.width, canvas.height - this.y + this.options.pipe_gap);
-        context.drawImage(pipe_up, 0, 0, pipe_up.width, pipe_up.height, this.x - delta, this.y + this.options.pipe_gap, pipe_up.width, this.options.pipe_top_height);
+        var x = this.x - delta;
+        var y = this.y + this.options.pipe_gap;
+        context.drawImage(pipe_image, x, y, pipe_image.width, canvas.height - y);
+        context.drawImage(pipe_up, x, y, pipe_up.width, this.options.pipe_top_height);
     };
 })(codefest = codefest || {});
